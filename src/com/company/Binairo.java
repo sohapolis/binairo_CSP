40eaad0a16e7a07dec24c190c2a4c5bab1ff3414
soha <sohapolis@gmail.com>
2022-04-20 21:19:02 +0430

    add AC3

--- src/com/company/Binairo.java
+++ src/com/company/Binairo.java
@@ -34,7 +34,9 @@ public class Binairo {
         //System.out.println("lcv color : "+LCV(state,0,2));
         //System.out.println(MRV(state)[0]+","+MRV(state)[1]);
         if (isConsistent(state)) {
-            backtrack(state);
+            //backtrack(state);
+            backtrackAC3(state,0);
+
             if (finalState==null) {
                 System.out.println("failed");
                 drawLine();
@@ -242,6 +244,72 @@ public class Binairo {
 
     }
 
+    private boolean AC3(State state){
+        //ArrayList<ArrayList<ArrayList<String>>> list=state.getDomain();
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <n ; j++) {
+                if (!state.getDomain().get(i).get(j).get(0).equals("n")){
+                    ArrayList<String> domain=new ArrayList<>();
+                    for (String d:state.getDomain().get(i).get(j)) {
+                        domain.add(d);
+                        State temp=state.copy();
+                        temp.getDomain().get(i).set(j,new ArrayList<>(List.of("n")));
+                        temp.getBoard().get(i).set(j,d.toUpperCase());
+                        if (forwardChecking(temp,i,j)==-1 || !checkIfUnique(temp)){
+                            domain.remove(d);
+                        }
+                    }
+                    if (domain.size()==0){
+                        return false;
+                    }
+                    state.getDomain().get(i).set(j,domain);
+                }
+            }
+
+        }
+        return true;
+    }
+
+    private boolean backtrackAC3(State state,int count){
+        if (isFinished(state)){
+            this.finalState=state;
+            return true;
+        }
+        int[] point=MRV(state);
+        String color=LCV(state,point[0],point[1]);
+        ArrayList<String> priority=new ArrayList();
+        for (String s:state.getDomain().get(point[0]).get(point[1])) {
+            if (s.equals(color)){
+                priority.add(0,s);
+            }
+            else {
+                priority.add(s);
+            }
+        }
+        /*
+        System.out.println(point[0]+","+point[1]);
+        System.out.println(color);
+        System.out.println(priority);
+
+         */
+        for (String s:priority) {
+            count++;
+            State temp=state.copy();
+            temp.getBoard().get(point[0]).set(point[1],s);
+            temp.getDomain().get(point[0]).set(point[1],new ArrayList<>(List.of("n")));
+            if (isConsistent(temp)) {
+                if (forwardChecking(temp,point[0],point[1])!=-1) {
+                    if (AC3(temp)) {
+                        if (backtrackAC3(temp,count)) {
+                            // this.finalState = temp;
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
     private boolean backtrack(State state){
         if (isFinished(state)){
@@ -371,7 +439,7 @@ public class Binairo {
             for (int k = j+1; k < n; k++) {
                 int count = 0;
                 for (int i = 0; i < n; i++) {
-                    if (cBoard.get(i).get(j).equals(cBoard.get(i).get(k))) {
+                    if (cBoard.get(i).get(j).equals(cBoard.get(i).get(k))&& !cBoard.get(i).get(j).equals("E")) {
                         count++;
                     }
                 }
@@ -380,7 +448,6 @@ public class Binairo {
                 }
             }
         }
-
         return true;
     }
 
@@ -396,7 +463,6 @@ public class Binairo {
         }
         return true;
     }
-
 
     private boolean isFinished(State state) {
         return allAssigned(state) && checkAdjacency(state) && checkNumberOfCircles(state) && checkIfUnique(state);
